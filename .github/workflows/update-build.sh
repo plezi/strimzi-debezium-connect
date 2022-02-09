f90ac81dc34fdd93fce1c8f4cf2c410f20ec9cdb
KOVACS Nicolas <perso.nkovacs@gmail.com>
2022-02-09 15:17:11 +0400

    fix: sh path and add more docs

--- .github/workflows/update-build.sh
+++ .github/workflows/update-build.sh
@@ -9,7 +9,7 @@ echo "$GITHUB_TOKEN" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
 
 echo "[INTERNAL] Sync plugins"
 
-../../../sync-plugins.sh
+./sync-plugins.sh
 
 echo "[DOCKER] Building images"
 docker build --cache-from ghcr.io/plezi/strimzi-debezium-connect:latest --build-arg BUILDKIT_INLINE_CACHE=1 -t ghcr.io/plezi/strimzi-debezium-connect:latest -t ghcr.io/plezi/strimzi-debezium-connect:"${1}" .
